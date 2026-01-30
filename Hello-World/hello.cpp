69e0407a23a9b6273062f25a6aa05f1dd68e249c
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-01-30 15:00:58 +0600

    Create hello.cpp

---
+++ Hello-World/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Welcome to Tahmid Alvee's GitHub!" << endl;
+    cout << "This is my first C++ program." << endl;
+    return 0;
+}
