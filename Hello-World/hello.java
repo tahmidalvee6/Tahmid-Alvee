23198f5e3442d0744d7cd7e4c65e9c43889aa91b
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-01-30 15:02:15 +0600

    Create hello.java

---
+++ Hello-World/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Welcome to Tahmid Alvee's GitHub!");
+        System.out.println("This is my first Java program.");
+    }
+}
